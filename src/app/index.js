c95daf138e66406f68a59d418a1989d176c31b44
Prataik <pratikm279@gmail.com>
2017-03-04 00:04:29 +0530

    Modularizing the code

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var DidComponent = require('./DidComponent');
+var AmComponent = require('./AmComponent');
+var WillComponent = require('./WillComponent');
 
 //Parent
 var ManageBooksComponent = React.createClass({
@@ -12,100 +15,6 @@ var ManageBooksComponent = React.createClass({
       </div>
     );
   }
-});
-
-var DidComponent = React.createClass({
-
-  getInitialState : function(){
-    return{
-      didArray : ['a','b','c']
-    };
-  },
-
-  render : function(){
-
-    var didRead = this.state.didArray.map(function(book, i){
-      return (
-        <li>{book}</li>
-      );
-    });
-
-    return (
-      <div>
-        <h2>Finished Reading</h2>
-        <ul>
-          {didRead}
-        </ul>
-      </div>
-    );
-  }, //render
-
-});
-
-var AmComponent = React.createClass({
-
-  getInitialState : function(){
-    return{
-      amArray : ['l','m','n','o']
-    };
-  },
-
-  render : function(){
-
-    var amRead = this.state.amArray.map(function(book, i){
-      return (
-        <li>{book}</li>
-      );
-    });
-
-    return (
-      <div>
-        <h2>Currently Reading</h2>
-        <ul>{amRead}</ul>
-      </div>
-    );
-  }
-});
-
-var WillComponent = React.createClass({
-
-  getInitialState : function(){
-    return{
-      willArray : ['p','q']
-    };
-  },
-
-  render : function(){
-    var willRead = this.state.willArray.map(function(book, i){
-      return (
-        <li>{book}</li>
-      );
-    });
-    return (
-      <div>
-        <h2>Wish to Read</h2>
-
-        <form id = "add-todo" onSubmit = {this.handleSubmit}>
-          <input type = 'text' id='willRead' placeholder = 'Add a Book' />
-          <input type = 'submit' value = 'Add Book' />
-        </form>
-
-        <ul>{willRead}</ul>
-      </div>
-
-    );
-  },
-
-  //custom functions
-  handleSubmit: function(e){
-    e.preventDefault();
-    this.state.willArray.push(document.getElementById('willRead').value);
-    console.log(this.state.willArray);
-    this.setState({
-      willArray : this.state.willArray
-    });
-  }
-
 });
 
 ReactDOM.render(<ManageBooksComponent />, document.getElementById('wrapper'));
