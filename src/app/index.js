0f6555c54f8da9586fd1b8257b9f0633672149b5
Prataik <pratikm279@gmail.com>
2017-03-08 00:35:35 +0530

    Integrating with Backend using Superagent

--- src/app/index.js
+++ src/app/index.js
@@ -2,47 +2,57 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 import _ from 'lodash';
 var CategoryBooksList = require('./CategoryBooksList');
+var request = require('superagent');
 
-var data = [
-  {
-    title: "a",
-    status: "finished_reading"
-  },
-  {
-    title: "b",
-    status: "want_to_read"
-  },
-
-  {
-    title: "c",
-    status: "currently_reading"
-  },
-
-  {
-    title: "e",
-    status: "finished_reading"
-  },
-
-  {
-    title: "f",
-    status: "dont_want_read"
-  },
-
-];
+// var data = [
+//   {title: "a", status: "finished_reading"},
+//   {title: "b", status: "want_to_read"},
+//   {title: "c", status: "currently_reading"},
+//   {title: "e", status: "finished_reading"},
+//   {title: "f", status: "dont_want_read"},
+// ];
+// var data;
+// request
+//    .get('http://127.0.0.1:8000/books/')
+//    .end(function(err, res){
+//       data = res.body;
+//       console.log('data', data);
+//    });
 
 var ManageBooks = React.createClass({
-  render : function(){
-    var status_books_dict = _.groupBy(data, 'status');
-    var category_list = _.map(status_books_dict, function(filtered_books, key){
-      return <CategoryBooksList books={filtered_books} status={key} key={key} />
-    })
 
+    getInitialState: function(){
+      return {data: {}}
+    },
+
+   componentDidMount: function(){
+     var self = this;
+     request
+        .get('http://127.0.0.1:8000/books/')
+        .end(function(err, res){
+           var data = res.body;
+           self.setState({data: data});
+        });
+   },
+
+   getBooks: function() {
+     if(this.state.data) {
+       var status_books_dict = _.groupBy(this.state.data, 'status');
+       var category_list = _.map(status_books_dict, function(filtered_books, key){
+         return <CategoryBooksList books={filtered_books} status={key} key={key} />
+       })
+       return category_list;
+     }
+    //  this.setState({category_list: category_list});
+   },
+
+     render : function(){
     return(
       <div>
-        {category_list}
+      {this.getBooks()}
       </div>
     )
-  }
+  },
 });
 
-ReactDOM.render(<ManageBooks data={data}/>, document.getElementById('wrapper'));
+ReactDOM.render(<ManageBooks />, document.getElementById('wrapper'));
